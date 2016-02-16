7a24b58208620a5dac232f32c50536ce5ef97cb9
Mikko <Mikko@ISO-Lenni.wlan.metropolia.fi>
2016-02-16 17:58:22 +0200

    Eka asennus

--- src/javaFXCollisionTOIMII/FXMain.java
+++ src/javaFXCollisionTOIMII/FXMain.java
@@ -3,26 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaFXpt2;
+package javaFXCollisionTOIMII;
 
-import javafx.animation.Animation;
+import javafx.animation.AnimationTimer;
 import javafx.animation.PathTransition;
 import javafx.application.Application;
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.SimpleDoubleProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.geometry.Point2D;
+import javafx.scene.DepthTest;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.HLineTo;
 import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
-import javafx.scene.shape.Shape;
 import javafx.scene.shape.VLineTo;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -33,54 +27,58 @@ import javafx.util.Duration;
  */
 public class FXMain extends Application {
 
-    private void checkBounds(Circle terve, Circle sairas) {
+    AnimationTimer animationTimer;
 
-        DoubleProperty xValue = new SimpleDoubleProperty();
-        xValue.bind(terve.centerXProperty());
-        xValue.addListener(new ChangeListener() {
+    // Havaitsee törmäyksen ja värjää ensimmäisenä annetun ympyrän punaiseksi
+    private void collision(Circle c1, Circle c2) {
+        animationTimer = new AnimationTimer() {
             @Override
-            public void changed(ObservableValue arg0, Object arg1, Object arg2) {
-
-                System.out.println(" Current center X : " + (double) arg2);
+            public void handle(long l) {
+                if (c1.getBoundsInParent().intersects(c2.getBoundsInParent())) {
+                    animationTimer.stop();
+                    c1.setFill(Color.RED);
+                }
             }
-        });
+        };
+        animationTimer.start();
     }
 
     @Override
     public void start(Stage primaryStage) {
 
-        Pane root = new Pane();
         Circle terve = new Circle(20, Color.LIGHTGREEN);
         Circle sairas = new Circle(20, Color.RED);
+        Group root = new Group(terve, sairas);
 
-        root.getChildren().addAll(terve, sairas);
+        root.setDepthTest(DepthTest.ENABLE);
 
         Path path = new Path();
-        path.getElements().addAll(new MoveTo(100, 300), new HLineTo(850));
+        path.getElements().addAll(new MoveTo(100, 200), new HLineTo(850));
         path.setFill(null);
         path.setVisible(false);
         root.getChildren().add(path);
 
         Path path2 = new Path();
-        path2.getElements().addAll(new MoveTo(500, 100), new VLineTo(850));
+        path2.getElements().addAll(new MoveTo(200, 100), new VLineTo(850));
         path2.setFill(null);
         path2.setVisible(false);
         root.getChildren().add(path2);
 
         Stage stage = new Stage();
 
-        Scene scene = new Scene(root, 800, 800);
+        Scene scene = new Scene(root, 800, 800, true);
         stage.setScene(scene);
+
+        collision(terve, sairas);
+        
         stage.show();
 
         PathTransition pt = new PathTransition(Duration.millis(4000), path, terve);
-        PathTransition pt2 = new PathTransition(Duration.millis(6000), path2, sairas);
+        PathTransition pt2 = new PathTransition(Duration.millis(4000), path2, sairas);
         //pt.setCycleCount(Animation.INDEFINITE);
         //pt.setAutoReverse(true);
         pt.play();
         pt2.play();
-
-        checkBounds(terve, sairas);
 
     }
 
